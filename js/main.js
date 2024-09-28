a5a8606d081888cfc3b7f5f58c845a6e0420de3f
Liubov <drogaltseva92@gmail.com>
2024-09-28 20:34:08 +0200

    add styles for cursor on mobile

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ window.addEventListener('mousemove', (event) => {
   gsap.to(cursor, {
     x: mouseX,
     y: mouseY,
-    duration: 0.2, // Час анімації, щоб курсор слідував плавно
+    duration: 0.2, 
     ease: 'power2.out'
   });
 });
@@ -30,42 +30,41 @@ window.addEventListener('mousemove', (event) => {
 
 
 
-// Знаходимо всі вогники
+
 const trails = document.querySelectorAll('.trail');
 
-// Масив позицій для вогників
+
 const positions = [];
 
-// Ініціалізуємо початкові позиції
+
 trails.forEach(() => positions.push({ x: 0, y: 0 }));
 
-// Відстежуємо курсор
+
 window.addEventListener('mousemove', (event) => {
   const { clientX: mouseX, clientY: mouseY } = event;
 
-  // Анімуємо кожен вогник з випадковою позицією навколо курсора
   trails.forEach((trail, index) => {
-    const angle = Math.random() * Math.PI * 2; // випадковий кут для позиції навколо курсора
-    const radius = Math.random() * 50 + 20; // випадковий радіус (відстань від курсора)
+    const angle = Math.random() * Math.PI * 2;
+    const radius = Math.random() * 50 + 20; 
     const x = mouseX + Math.cos(angle) * radius;
     const y = mouseY + Math.sin(angle) * radius;
 
     gsap.to(positions[index], {
       x, y,
-      duration: 0.8 + index * 0.05, // Більша затримка для плавності
+      duration: 0.8 + index * 0.05,
       ease: "power3.out",
       onUpdate: () => {
         gsap.set(trail, {
-          x: positions[index].x - 7.5, // корекція, щоб центр елемента був на позиції
+          x: positions[index].x - 7.5, 
           y: positions[index].y - 7.5
         });
       }
     });
 
-    // Прозорість і розмір змінюються для кожного вогника
+
     gsap.to(trail, {
-      opacity: 0.6 - index * 0.05, // поступове зникання
-      scale: 1 - index * 0.05,     // менші вогники далі від курсора
+      opacity: 0.6 - index * 0.05, 
+      scale: 1 - index * 0.05,   
       duration: 1
     });
   });
